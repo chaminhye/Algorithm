ebb52cc56195b82e347761156a7cbb191b1905b1
mhcha <mhcha@m-sync.co.kr>
2020-06-26 19:00:54 +0900

    탐욕번 1번 문제

---
+++ src/greedy/Greedy1.java
@@ -0,0 +1,27 @@
+package greedy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Greedy1 {
+	
+	public static void main(String args[]) {
+		int n = 5;
+		int[] lost = {2,4};
+		int[] reserve = {1,3,5};
+		
+		List<Integer> studentList = new ArrayList<>();
+		for(int i=1; i<= n; i++) {
+			studentList.add(i);
+		}
+
+		for(int i=0; i<lost.length; i++) {
+			for(int j=0; j<reserve.length;j++) {
+				if(lost[i] == reserve[j] +1 || lost[i] == reserve[j] -1) {
+					
+				}
+			}
+		}
+		
+	}
+}
