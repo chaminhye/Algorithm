169f27faebc63817fdedd3feb16e092e523a8b9c
mhcha <mhcha@m-sync.co.kr>
2020-06-26 12:43:36 +0900

    완전탐색4번 풀이

--- src/bruteForce/BP4.java
+++ src/bruteForce/BP4.java
@@ -1,54 +1,60 @@
 package bruteForce;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BP4 {
 	static public void main(String args[]) {
-		int brown = 24;
-		int yellow = 24;
+//		int brown = 24;
+//		int yellow = 24;
+		int brown = 10;
+		int yellow = 2;
+		
 		int[] answer = {0,0};
 		
 		int area = brown + yellow;
 		answer = divisor(area, yellow);
-		System.out.println(answer[0]);
+		System.out.println(answer[0]+" / "+answer[1]);
 	}
 	
 	public static int[] divisor(int area, int yellow ) {
-		Map<Integer, Integer> areaMap = new HashMap<Integer, Integer>();
-		Map<Integer, Integer> yellowMap = new HashMap<Integer, Integer>();
+		
+		
+		List<List<Integer>> areaList = new ArrayList<>();
+		List<List<Integer>> yellowList = new ArrayList<>();
 		
 		int[] result = {0,0};
 		
-		for(int i =3; i<=area/2 ; i++) {
-			if(area % i == 0) {
-				if(i!=1 && i!=2 && i!= area/2) {
-					areaMap.put(i, area/i);
+		for(int i =3; i<=area ; i++) {			// yellow가 존재하면 최소높이는 3부터 시작함	
+			if(area % i == 0) {					// 전체 넓이의 약수 구하기
+				if(area/i != 1 && area/i != 2) {
+					List<Integer> innerList = new ArrayList<Integer>();
+					innerList.add(i);
+					innerList.add(area/i);
+					areaList.add(innerList);
 				}
 			}
-			areaMap.remove(area/i, i);
+		}
+		for(int i =1; i<=yellow ; i++) {			
+			if(yellow % i == 0) {				// yellow 넓이의 약수 구하기
+				List<Integer> innerList = new ArrayList<Integer>();
+				innerList.add(i);
+				innerList.add(yellow/i);
+				yellowList.add(innerList);
+			}
 		}
 		
-		for(int i =1; i<=yellow/2 ; i++) {
-			if(yellow % i == 0) {
-				if(i!=1 && i!= yellow/2) {
-					yellowMap.put(i, yellow/i);
-				}
-			}
-			yellowMap.remove(yellow/i, i);
-		}
-System.out.println("areaMap : "+areaMap);		
-System.out.println("yellowMap : "+yellowMap);		
-		for(int i =0;i<areaMap.size();i++) {
-			for(int j=0; j<yellowMap.size(); j++) {
-			System.out.println(areaMap.keySet() +" / "+ yellowMap.get(i));	
-				if(areaMap.get(i) >= yellowMap.get(i)+2 && areaMap.get(i).intValue() >=  yellowMap.get(i).intValue()+2) {
-					result[0] = areaMap.get(i);
-					result[1] = areaMap.get(i).intValue();
+		for(int i =0;i<areaList.size();i++) {			
+			for(int j=0; j<yellowList.size(); j++) {
+
+				// 총 너비 = (옐로우 너비+2) && 총 높이 = (옐로우 높이+2) 
+				if(areaList.get(i).get(0) == (yellowList.get(j).get(0)+2) && areaList.get(i).get(1) == (yellowList.get(j).get(1)+2)) {
+					result[0] = (int) areaList.get(i).get(0);
+					result[1] = (int) areaList.get(i).get(1);
 				}
 			}
 		}
+		
 		return result;
-		
 	}
 }
