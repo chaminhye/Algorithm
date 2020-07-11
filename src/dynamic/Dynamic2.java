632027ea59907755f0336d8ad8051d144eeb9742
mhcha <mhcha@m-sync.co.kr>
2020-07-11 13:30:19 +0900

    동적계획법2번 문제 풀이완료

--- src/dynamic/Dynamic2.java
+++ src/dynamic/Dynamic2.java
@@ -20,33 +20,20 @@ public class Dynamic2 {
 			6	42
 	 * */
 	
-	static int[] fibo_arr;
 	public static void main(String[] args) {
-		int N = 5;
-		fibo_arr = new int[N+1];
-		//fibo(5);
-//		System.out.println(fibo_arr[3]);
-//		System.out.println(fibo_arr[4]);
-//		System.out.println(fibo_arr[5]);
-		long answer = (fibo_arr[4] + fibo_arr[5]) * 2;
 		System.out.println(fibo(6));
 	}
-/*	
-	static void fibo(int num) {
-		fibo_arr[0] = 1;
-		fibo_arr[1] = 1;
-
-		for(int i=2; i<=num;i++) {
-			fibo_arr[i] = fibo_arr[i-2] + fibo_arr[i-1];
-		}
-//		return fibo(num-1) + fibo(num-2);
-	}
-	*/
-	static int fibo(int num) {
-		int first = 1;
-        int second = 1;
+	
+	static long fibo(int num) {
+		// N >= 80이면, int로는 표현 불가
+		long first = 1;
+		long second = 1;
         
-        int answer = 0;
+        if(num <= 1) {
+        	return 1;
+        }
+        
+        long answer = 0;
 
 		for(int i=1; i<num;i++) {
 			answer = first + second;
