05d5c809b1a6076217dcd344e90f8b9878ac7c9f
mhcha <minhye_love@naver.com>
2020-09-20 15:38:39 +0900

    정렬2 문제 풀이중

---
+++ src/programmers/sort/Sort2.java
@@ -0,0 +1,81 @@
+package programmers.sort;
+
+
+/*
+ * 문제 설명
+		0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+		
+		예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+		
+		0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+	
+	제한 사항
+		numbers의 길이는 1 이상 100,000 이하입니다.
+		numbers의 원소는 0 이상 1,000 이하입니다.
+		정답이 너무 클 수 있으니 문자열로 바꾸어 return 합니다.
+		
+	입출력 예
+		numbers	            return
+		[6, 10, 2]	         6210
+		[3, 30, 34, 5, 9]	9534330
+		
+	순열 참고  : https://bcp0109.tistory.com/14
+*/
+public class Sort2 {
+
+	public static void main(String args[]) {
+		
+		int[] numbers = {6 ,10,2};
+		int n = numbers.length;
+		
+		permutation(numbers,  0 , n, n);
+		
+	}
+	
+	
+	// 순서 없이 n 개중에서 r 개를 뽑는 경우
+	// 사용 예시: permutation(arr, 0, n, r);
+    static void permutation(int[] arr, int depth, int n, int r) {
+        if (depth == r) {
+            print(arr, r);
+            return;
+        }
+
+        for (int i = depth; i < n; i++) {
+            swap(arr, depth, i);
+            permutation(arr, depth + 1, n, r);
+            swap(arr, depth, i);
+        }
+    }
+    
+    static void swap(int[] arr, int depth, int i) {
+        int temp = arr[depth];
+        arr[depth] = arr[i];
+        arr[i] = temp;
+    }
+    
+    // 배열 출력
+    static void print(int[] arr, int r) {
+    	String str = "";
+    	int number = 0;
+    	int max = 0;
+    	
+    	for(int i: arr) {
+    		str += i;
+    		number = Integer.parseInt(str);
+    		System.out.println("num : "+i+" - "+number);
+    		if(number > max) {
+    			max = number;
+    		}
+    	}
+    	
+    	System.out.println("max : "+max );
+    	
+		/*
+		 * System.out.println("# : "+result[0]);
+		 * 
+		 * for (int i = 0; i < r; i++) { System.out.print(arr[i] + " "); result[i] +=
+		 * arr[i] + " "; } System.out.println();
+		 */
+    }
+}
