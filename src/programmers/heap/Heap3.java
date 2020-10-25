ca9b6acbfbf93fcda32163b388fbbda8e1517b82
mhcha <minhye_love@naver.com>
2020-10-25 22:16:04 +0900

    heap 3번 문제 풀이 완료

--- src/programmers/heap/Heap3.java
+++ src/programmers/heap/Heap3.java
@@ -1,5 +1,6 @@
 package programmers.heap;
 
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
@@ -33,69 +34,45 @@ public class Heap3 {
 	
 	public static int[] solution(String[] operations) {
 		int[] answer = new int[2];
+
+		// Key Point
+		// 최소 우선순위큐, 최대 우선순위큐를 사용하여 , 각각의 최대 ,최소값을 추출한다.
+		PriorityQueue<Integer> minQ = new PriorityQueue<Integer>();
+		PriorityQueue<Integer> maxQ = new PriorityQueue<Integer>(Comparator.reverseOrder());
 		
-		PriorityQueue<Integer> queue = new PriorityQueue<>();
-        int max = 0;
-        int min = 0;
-        int number = 0;
-        int temp = 0;
+		// operations 배열을 돌면서, I가 포함된 경우 queue에 삽입, D 가 들어간 경우 최소,최대 큐 동시에 삭제
 		for(String str : operations) {
-			number = Integer.parseInt(str.substring(2, str.length()));
-			
-			if(str.contains("I")) {				// I : 큐에 주어진 숫자를 삽입	
-				queue.offer(number);
-				if( number > max) {
-					temp = min;
-					//min = number;
-					max = number;
-				}else {
-					temp = min;
-					min = number;
-					//max = temp;
+			if(str.contains("I")) {		// insert minQ, maxQ
+				minQ.offer(Integer.parseInt(str.replace("I ", "")));
+				maxQ.offer(Integer.parseInt(str.replace("I ", "")));
+			}else if(str.contains("D 1")) {		
+				if(!maxQ.isEmpty()) {
+					minQ.remove(maxQ.peek());
+					maxQ.poll();
 				}
-				System.out.println("min : "+min+ " / max : " + max+ " / number : " + number);
-			}else if(str.contains("D -1")){		// D -1 : 큐에서 최솟값 삭제 
-				System.out.println("	min : "+ min);
-				if(!queue.isEmpty()) {
-					queue.remove(min);
-					min = max;
-				}
-			}else if(str.contains("D 1")){								// D 1 : 큐에서 최댓값 삭제
-				System.out.println("	max : "+ max);
-				if(!queue.isEmpty()) {
-					queue.remove(max);
-					max = min;
+			}else if(str.contains("D -1")) {
+				if(!minQ.isEmpty()) {
+					maxQ.remove(minQ.peek());
+					minQ.poll();
 				}
 			}
+//			System.out.println("str : "+str +" / minQ : "+minQ + " / maxQ :"+maxQ);
 			
-			if(queue.isEmpty()) {				// 큐가 비어 있는 경우
-				queue.offer(0);
-			}
 		}
-		System.out.println("queue cnt : "+queue.size());
-		System.out.println("queue : "+queue);
 		
-		if(queue.size() == 0) {
+		if(!maxQ.isEmpty() && !minQ.isEmpty()) {
+			answer[0] = maxQ.peek();
+			answer[1] = minQ.peek();
+		}else {
 			answer[0] = 0;
 			answer[1] = 0;
-		}else {		
-			answer[1] = queue.poll();
-			while(!queue.isEmpty()) {
-				queue.poll();
-				if(queue.size() == 1) {
-					answer[0] = queue.poll();
-				}				
-			}
-			
-			System.out.println(answer[0]);
-			System.out.println(answer[1]);
-		}	
+		}
 		
 		return answer;
 	}
 	
 	public static void main(String args[]) {
-		String[] operations = {"I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6"}; 
+		String[] operations = {"I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"}; 
 //		String[] operations = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
 //		String[] operations = {"I 16","I 6", "D 1","I 160"};
 
