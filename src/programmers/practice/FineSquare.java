fb3b9f445f81f17c07a92167567ffe3455af8946
chaminhye <minhye_love@naver.com>
2020-12-29 14:47:46 +0900

    Update FineSquare.java
    멀쩡한 사각형 문제풀이 수정

--- src/programmers/practice/FineSquare.java
+++ src/programmers/practice/FineSquare.java
@@ -10,6 +10,10 @@ package programmers.practice;
  *  	2. 최대공약수라는 힌트를 얻었지만, 그래서,,,그 다음은 뭐..지?
  *      3. 제거 해야되는 사각형의 규칙을 구하자!! (아래 블로그 참고)
  *      	https://m.blog.naver.com/tlstjd436/221849619470
+ *      4. long 타입.........주의 모든 w*h계산식엔 다 long 타입변환!!
+ *      
+ *  자세한 문제 풀이 내용은   
+ *  	-> https://velog.io/@ming/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-%EB%A9%80%EC%A9%A1%ED%95%9C-%EC%82%AC%EA%B0%81%ED%98%95Level-2
  * */
 public class FineSquare {
 
@@ -24,17 +28,22 @@ public class FineSquare {
         if(w == 1 || h == 1){		// 가로 또는 세로의 길이가 1인경우 사각형의 개수 0
             return 0;
         }
-        if(w == h){					// 가로= 세로 길이가 같은 정사각형 인 경우 가로갯수만큼만 제외하면 됨
-            return (w*h) - w ;
+        long wl = (long) w;			// 모든 계산을 long타입으로 하기 위해 미리 형변환
+        long hl = (long) h;
+        
+        if(wl == hl){				// 가로= 세로 길이가 같은 정사각형 인 경우 가로갯수만큼만 제외하면 됨
+            return (wl*hl) - wl ;
         }
         
-        long r = gcd((long)w,(long)h);
+        long r = gcd(wl,hl);		// 최대공약수
         
-        answer = w*h  - ((w/r) + (h/r) -1 ) * r;
+        // 직사각형(w*h)의 넓이 - (제거할 사각형의 개수 * 최대공약수 개수)
+        answer = wl*hl  - ((wl/r) + (hl/r) -1 ) * r;
         
 		return answer;
 	}
 	
+	/** 유클리드 호제법 - 최대공약수 구하기 */
     public static long gcd(long w, long h){
         while(h!=0){
             long r= w%h;
